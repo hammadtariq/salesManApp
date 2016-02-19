207f1ee1c7380bbaa2b59ff699a2ff4822b912f8
ziaukhan <ziaukhan@hotmail.com>
2016-02-19 21:08:18 +0500

    login and signup working now (reverted from commit ada524c22e3fd294556618e8338c91054c794c3a)

--- client/app/boot.ts
+++
@@ -1,9 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser'
-import {provide} from 'angular2/core'
-import {AppComponent} from './app'
-import {ROUTER_DIRECTIVES,
-  ROUTER_PROVIDERS,
-  LocationStrategy,
-  HashLocationStrategy} from 'angular2/router';
-import{HTTP_BINDINGS} from 'angular2/http';
-bootstrap(AppComponent,[HTTP_BINDINGS,ROUTER_PROVIDERS,provide(LocationStrategy, {useClass: HashLocationStrategy})]);
